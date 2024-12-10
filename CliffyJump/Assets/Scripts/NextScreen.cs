0f145f8a2987c1f02699bbb7c5322e15952c5383
Jon <jon.campillo@gmail.com>
2024-12-10 18:20:44 +0100

    Three screens of level 1

---
+++ CliffyJump/Assets/Scripts/NextScreen.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class NextScreen : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject[] screens = new GameObject[10];
+
+    private int currentScreen = -1;
+
+    public void LoadNextScreen() 
+    {
+        // No more screens, we have beaten the level
+        if (++currentScreen == 10) 
+        {
+            return;
+        }
+
+        // More screens, load the next
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
