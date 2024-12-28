ab64f505fb7d65eefc5893be4c6766f1b56d48b8
bunnerd <sandravaro03@gmail.com>
2024-12-28 02:41:58 +0100

    Show level progress

--- CliffyJump/Assets/Scripts/UI/UIManager.cs
+++ CliffyJump/Assets/Scripts/UI/UIManager.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -6,9 +7,14 @@ public class UIManager : MonoBehaviour
 {
     public Button exitGameButton;
 
+    public TextMeshProUGUI sakuraProgress;
+    public TextMeshProUGUI ruinsProgress;
+
     void Start()
     {
         exitGameButton.onClick.AddListener(ExitGame);
+        sakuraProgress.text = PlayerPrefs.GetInt("maxProgress" + 0, 0).ToString() + "%";
+        ruinsProgress.text = PlayerPrefs.GetInt("maxProgress" + 1, 0).ToString() + "%";
     }
 
     private void ExitGame()
